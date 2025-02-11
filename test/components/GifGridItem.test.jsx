c019bace5f22f47a1cb20b8ffb70585af1fcef65
Ismael Aguilera <iaguilera@aguilerafactory.mx>
2025-02-10 19:38:55 -0600

    resolviendo el problema del file sistem

---
+++ test/components/GifGridItem.test.jsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react"
+import { GifGridItem } from "../../src/components/GifGridItem"
+
+describe('Pruebas en <GifGridItem />', () => { 
+    const title = 'Saitama';
+    const url='https://one-punch.com/saitama.jpg';
+    
+    test('debe de hacer match con el snapshot', () => {     
+        const { container } = render( <GifGridItem title={title} url = { url } /> )
+        expect( container ).toMatchSnapshot();
+     });
+ })
