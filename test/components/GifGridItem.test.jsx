ef63afdb15e5d1034e277433d69a6f730ecf1b0f
Ismael Aguilera <iaguilera@aguilerafactory.mx>
2025-02-11 11:33:05 -0600

    se resuelve los problemas de las pruebas

--- test/components/GifGridItem.test.jsx
+++ test/components/GifGridItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { GifGridItem } from '../../src/components/GifGridItem'
 
 describe('Pruebas en <GifGridItem />', () => { 
@@ -6,7 +6,21 @@ describe('Pruebas en <GifGridItem />', () => {
     const url='https://one-punch.com/saitama.jpg';
     
     test('debe de hacer match con el snapshot', () => {     
-        const { container } = render( <GifGridItem title={title} url = { url } /> )
+        const { container } = render( <GifGridItem title={title} url = { url } /> );
         expect( container ).toMatchSnapshot();
      });
- })
+
+     test('debe de mostrar la imagen con el URL y el ALT Indicado', () => {
+        render( <GifGridItem title={title} url = { url } /> );
+        //screen.debug();
+        const {src, alt} = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+
+     });
+
+     test('debe de mostrar el titulo en el componente', () => { 
+        render( <GifGridItem title={title} url = { url } /> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+      })
+ });
