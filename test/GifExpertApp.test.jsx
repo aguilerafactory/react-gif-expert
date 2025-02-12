913bf3a6d0940e9cb2b82e149d1c18adb1261128
Ismael Aguilera <iaguilera@aguilerafactory.mx>
2025-02-11 18:57:05 -0600

    Se termina la seccion de pruebas

---
+++ test/GifExpertApp.test.jsx
@@ -0,0 +1,16 @@
+import { screen } from "@testing-library/dom";
+import { GifExpertApp } from "../src/GifExpertApp";
+import { render } from "@testing-library/react";
+
+describe('Pruebas en <GifExpertApp />', () => {
+
+    test('should ', () => {
+        
+        render( <GifExpertApp /> );
+        screen.debug();
+
+
+    });
+
+
+});
