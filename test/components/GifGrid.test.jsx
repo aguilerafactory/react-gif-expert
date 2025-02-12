249f7e1d4608e0b6f6cdb455584dc9db40ee4106
Ismael Aguilera <iaguilera@aguilerafactory.mx>
2025-02-11 18:07:51 -0600

    se terminan todas las pruebas de los componentes

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from '../../src/components/GifGrid'
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => { 
     const category = 'Hola';
 
     test('debe de mostrar el loading inicialmente', () => { 
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true,
+        });
+
         render( <GifGrid category= { category } />);
         expect( screen.getByText( 'Cargando...'));
         expect( screen.getByText( category));
@@ -12,7 +20,26 @@ describe('Pruebas en <GifGrid />', () => {
      });
 
      test('debe de mostrar items cuando se cargan las imagenes useFetchGifs', () => { 
-        
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg',
+            },
+            {
+                id: 'a123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg',
+            },
+        ];
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: true,
+        });
+
+        render( <GifGrid category= { category } />);
+        expect( screen.getAllByRole('img').length ).toBe(2);
+
       });
 
  });
